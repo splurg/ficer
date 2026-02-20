82949572df8fb3fa72d78f0a53f68495b694eb62
kai <kai.donegan@gmail.com>
2026-02-19 20:56:48 -0500

    more things

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var GHPATH = '/ficker';
 
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register(
-        '${GHPATH}/sw.js',
-        { scope: '${GHPATH}/' }
+        `${GHPATH}/sw.js`,
+        { scope: `${GHPATH}/` }
     )
 }
